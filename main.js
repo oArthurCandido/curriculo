d1bdee78983e1d26810565660cedd52ceb56e48f
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-04-30 12:01:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/* mudar o header da páguina quando der scroll */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
